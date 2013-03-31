d6822e806f81f252769d7b2b57365f74acfa9289
Tammam Kbeili <tammam.kbeili@gmail.com>
2013-03-31 15:38:24 -0700

    first commit

---
+++ spec/omniauth/strategies/evernote_spec.rb
@@ -0,0 +1,7 @@
+unless RUBY_PLATFORM == 'java' 
+  require File.expand_path('../../../spec_helper', __FILE__)
+
+  describe OmniAuth::Strategies::Evernote do
+    it_should_behave_like "an oauth strategy"
+  end
+end
